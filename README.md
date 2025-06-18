be1eb9c692db1db67e10ead1d96ea9b58b5f1104
Rishabh K <ksrsk2002@gmail.com>
2025-06-19 08:36:12 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#README
+
+A quick way to set up vim for JS development.
+
+The setup script will get updated as and when I discover more awesome stuff to use with vim!
+
+A shout out and special thanks to the following github users -
+* tpope
+* beautify-web
+* jiangmiao
+* kristijanhusak
+* crusoexia
+* pangloss
---
+++ setup.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# VIM
+curDir=`pwd`
+backupDir=vim-`date +%Y%m%d_%H%M%S`
+cd ~
+mkdir $backupDir
+mv ~/.vim* $backupDir/
+mv $backupDir ".bkp-$backupDir"
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle  ~/.vim/colors ~/.vim/plugin ~/.vim/syntax
+
+# Pathogen
+curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+#Prettier
+git clone https://github.com/prettier/vim-prettier ~/.vim/bundle/vim-prettier
+cd ~/.vim/bundle/vim-prettier/
+npm install
+cd ~
+
+# Auto Pairs
+git clone https://github.com/jiangmiao/auto-pairs.git
+mv auto-pairs/plugin/auto-pairs.vim ~/.vim/plugin/
+rm -rf ~/auto-pairs
+
+# VIM JS
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
+git clone https://github.com/crusoexia/vim-javascript-lib.git
+mv ~/vim-javascript-lib/after/syntax/javascript.vim ~/.vim/syntax/
+rm -rf ~/vim-javascript-lib/
+
+# TComment
+git clone https://github.com/tomtom/tcomment_vim.git ~/.vim/bundle/tcomment
+
+# Git Gutter
+git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+
+# Supertab
+git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
+
+# Vum TS
+git clone https://github.com/leafgarland/typescript-vim ~/.vim/bundle/typescript-vim
+
+# Colorschemes
+cp $curDir/colors/* ~/.vim/colors/
+
+# Git
+rm -rf ~/.gitconfig
+rm /usr/local/bin/icdiff
+curl -o icdiff https://raw.githubusercontent.com/jeffkaufman/icdiff/release-1.8.1/icdiff
+chmod ugo+rx icdiff
+mv icdiff /usr/local/bin/
+cd $curDir
+cp gitconf ~/.gitconfig
+
+# vimrc
+cp $curDir/dotfiles/vimrc ~/.vimrc
+
+# bashrc
+cp $curDir/dotfiles/bashrc-local ~/.bashrc
+
+# tmux.conf
+cp $curDir/dotfiles/tmux.conf ~/.tmux.conf
